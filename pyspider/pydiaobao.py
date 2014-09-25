2c1de7ca10760c856a6cf206bdfe2584da9ea5c4
omengye <ym2008love@gmail.com>
2014-09-25 22:07:44 +0800

    fix bug

--- pyspider/pydiaobao.py
+++ pyspider/pydiaobao.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 import urllib2
 import re
 import bs4
@@ -11,11 +11,7 @@ urls.append(url)    # 以列表形式呈现所有页数的url
 class SimpleCookieHandler(urllib2.BaseHandler): # 添加自定义cookie
 
     def http_request(self, req):
-        simple_cookie = 'SUID=8614F37266CA0D0A0000000053A6EB57; \
-        SUV=1403448156290881; CXID=D570E9D33EF902902AAEE5245112E2EC; \
-        ssuid=6218322088; pgv_pvi=4723382272; IPLOC=CN1100; \
-        SNUID=333D81037074725FAC62E98A71545729; sct=3; ABTEST=2|1410874685|v1; \
-        LSTMV=362%2C506; LCLKINT=4268'
+        simple_cookie = 'SUID=8614F37266CA0D0A0000000053A6EB57; SUV=1403448156290881; CXID=D570E9D33EF902902AAEE5245112E2EC; ssuid=6218322088; pgv_pvi=4723382272; sct=5; LSTMV=215%2C288; LCLKINT=6490; SNUID=09B247C6B5B1B66A660F93ECB515CB94; PHPSESSID=h0mvenu4sgela33q7s5145f920; SUIR=09B247C6B5B1B66A660F93ECB515CB94; IPLOC=CN1101; ABTEST=3|1411634745|v1'
 
         if not req.has_header('Cookie'):
             req.add_unredirected_header('Cookie', simple_cookie)
@@ -40,6 +36,8 @@ def find_page_maxNo(html_content = None):
 
 maxNo = find_page_maxNo(get_html_content(url))
 
+print u'总页数' + str(maxNo)
+
 # 查找并替换url中page的值
 def replace_url(url=None, Max=1):
     rule = re.compile('page\=\d\&')
@@ -49,6 +47,7 @@ def replace_url(url=None, Max=1):
     return urls
 
 urls = replace_url(url, maxNo+1)
+
 
 def find_key_url(html_content = None):
 
@@ -82,7 +81,8 @@ for page_url in urls:
     key_url = find_key_url(content)
     key_urls.extend(key_url)
 
-# print key_urls
+print u'Items Num:' + str(len(key_urls))
+
 
 def get_pic_url(url=None):  # 挑出页面中所有的图片链接
     page_content = get_html_content(url)
@@ -111,19 +111,16 @@ for i in key_urls:
     all_pic_urls.extend(get_pic_url(i))
     time.sleep(5)   # 间隔5秒采集一次
 
-# print real_pic_urls
+print u'查找结束'
 
 final_urls = filter(lambda x : all_pic_urls.count(x) == 1, all_pic_urls) # 删除所有重复项
 
-ads_url = [u'http://mmbiz.qpic.cn/mmbiz/ibfSwYkJfj0szae7EVSRVB17MttuDmyeAiaDGIYB3nSsdqFEqqTBOUXLUljbTylbcwBHybrKlg3d1gIKL1TCecKw/0', \
-           u'http://mmbiz.qpic.cn/mmbiz/ibfSwYkJfj0uOajPmwUAfTcOKGgMgZ8WWicObJxCfRrDffFZ5ntsHvLVW7wD0nQmndXVbGCIjkVqEFNZmbNCFOnQ/0', \
-           u'http://mmbiz.qpic.cn/mmbiz/ibfSwYkJfj0v8EXQ8cvK4NraibD8bJapPqPTO7lTRI46y7AglS9r3sibqmU1CTu0y47uBMLAflmDGPezK7l7ZeTfg/0', \
-           u'http://mmbiz.qpic.cn/mmbiz/ibfSwYkJfj0tMMqX2jw1lxiawwP8vEsmkib8Y8lr1V6utbIV69ggtZN0EAnWKCrWbjfiabl2fIicjKoqMtpYXEeyqVg/0', \
-           u'http://mmbiz.qpic.cn/mmbiz/ibfSwYkJfj0vqRiayu7DS2K3ugicZPCsibibJCqLuUfVvhiabXh9B5OSFRp6QJojiaId9O3Gr5ERrak4Lzr40mWFXLbbg/0']
+ads_url = [u'http://mmbiz.qpic.cn/mmbiz/ibfSwYkJfj0szae7EVSRVB17MttuDmyeAiaDGIYB3nSsdqFEqqTBOUXLUljbTylbcwBHybrKlg3d1gIKL1TCecKw/0', u'http://mmbiz.qpic.cn/mmbiz/ibfSwYkJfj0uOajPmwUAfTcOKGgMgZ8WWicObJxCfRrDffFZ5ntsHvLVW7wD0nQmndXVbGCIjkVqEFNZmbNCFOnQ/0', u'http://mmbiz.qpic.cn/mmbiz/ibfSwYkJfj0vhRQTTKMfXFewrcBNmSkCNlKK0Z1cgG5vsE1hWahv7FcPLtQKohl7P8FsAib6egwYLalhVDMfKclQ/0', u'http://mmbiz.qpic.cn/mmbiz/ibfSwYkJfj0uAZrg1531grGPKBMTxGl10iceKiaYuaUacLpyur6rjCSia1VHGGgBnMWmP9ZkWSGWBKfsoa2YAicv27A/0', u'http://mmbiz.qpic.cn/mmbiz/ibfSwYkJfj0vrmzIlOraTIBCMoj9gTPgERXdgiaOKYibA5NicgqwACHNZ2ZPWE8rxrNOQVqtpLQLvfzX6pNNbut2bg/0', u'http://mmbiz.qpic.cn/mmbiz/ibfSwYkJfj0uThUx5P4hF3SD4mHfuEYENUib0yclhrrTxygnOebNI8T71q7sqHsgNZjc8ENJQGiaAqxX3GA6C8mWg/0', u'http://mmbiz.qpic.cn/mmbiz/ibfSwYkJfj0sMtKicPnL9DcJDaGXz1r0CbyOuSLgm53iaC3DQibKibjIddP1L4JoAD5hibRTKMApbKCuEa9oMFNyQPEw/0', u'http://mmbiz.qpic.cn/mmbiz/ibfSwYkJfj0vVCr7JCPxTkoPwjx5XFRfoQGjLrG3Ra0iaiawH8kY0T8SBhpnLTAmibMyVJ6kuxWycrzlOzibPib8Zewg/0', u'http://mmbiz.qpic.cn/mmbiz/ibfSwYkJfj0vawYnPZgcqJHBArOz9fgFDJDuo7RLIkeyzPRSYo9eQKVk1tTGN9ibHtdkbpVFicMV6jhgmj4UjJZhA/0', u'http://mmbiz.qpic.cn/mmbiz/ibfSwYkJfj0sDIdtelVgrTkpCbHXEChpT6CcdzIXFibrjcqnBGUH9kRPRR5FGKPD0O8xibGfrfPJsSbVfic0wh6F6w/0', u'http://mmbiz.qpic.cn/mmbiz/ibfSwYkJfj0vvbiaqjd2NUjRBBPIHfvfFdXbvvebF6iciaoCPQt0k0kKp4QjVEssU5kHlUEtibQosTQ0V0oNx76o3Tw/0', u'http://mmbiz.qpic.cn/mmbiz/ibfSwYkJfj0se9zDkaAfBNpHlhWU1AwD3eiaBZiaiblloW1VkKWyTgg8Gq3dX8GusqZoX2XgYekicn1oPwxm7foVdeA/0']
 
 
 for ad_url in ads_url:
-    del_ad = final_urls.index(ad_url)
-    del final_urls[del_ad]  # 删除广告图片
+    if ad_url in final_urls:
+        del_ad = final_urls.index(ad_url)
+        del final_urls[del_ad]  # 删除广告图片
 
 print final_urls
